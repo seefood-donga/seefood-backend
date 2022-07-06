899cec6b67ff7a65e339c2a60e7e3cb2f16a9622
dkswnkk <jjoy9810@gmail.com>
2022-07-06 21:41:57 +0900

    Initial commit

---
+++ src/main/java/SenierProject/SeeFood/SeefoodApplication.java
@@ -0,0 +1,13 @@
+package SenierProject.SeeFood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeefoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeefoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SenierProject/SeeFood/SeefoodApplicationTests.java
@@ -0,0 +1,13 @@
+package SenierProject.SeeFood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeefoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
